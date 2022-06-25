82560916ead9a12777172e6182357beedd211ab5
ken551 <hide.delorean@gmail.com>
2022-06-25 21:55:11 +0900

    現在時刻を表示

--- kousu.py
+++ kousu.py
@@ -1,10 +1,14 @@
 import tkinter as tk
+import datetime
 
 # ウィンドウサイズ設定
 WINDOW_STANDBY_WIDTH = 400
 WINDOW_STANDBY_HEIGHT = 300
 WINDOW_COUNTING_WIDTH = 160
 WINDOW_COUNTING_HEIGHT = 120
+
+SYSTEM_FONT_FAMILY = "Arial"
+SYSTEM_FONT_SIZE = 30
 
 def startCounting():
     root.geometry(f"{WINDOW_COUNTING_WIDTH}x{WINDOW_COUNTING_HEIGHT}")
@@ -15,11 +19,28 @@ def startCounting():
     root.attributes("-topmost", True)
     frameCounting.tkraise()
 
+    global labelTime
+    timeNow = datetime.datetime.now()
+    labelTime["text"] = timeNow.strftime("%H:%I:%S")
+    root.after(500, refreshCounter)
+
+def refreshCounter():
+
+    global isColonDisplayed
+    if isColonDisplayed:
+        labelTime["text"] = datetime.datetime.now().strftime("%H %I %S")
+        isColonDisplayed = False
+    else:
+        labelTime["text"] = datetime.datetime.now().strftime("%H:%I:%S")
+        isColonDisplayed = True
+    root.after(500, refreshCounter)
+    
+
+
 def stopCounting():
     root.geometry(f"{WINDOW_STANDBY_WIDTH}x{WINDOW_STANDBY_HEIGHT}")
     # ステータスバー表示
     root.overrideredirect(0)
-    isStatusBarShown = True
     # 最前面固定解除
     root.attributes("-topmost", False)
     frameStandby.tkraise()
@@ -30,6 +51,9 @@ def onExit():
 if __name__ == '__main__':
     root = tk.Tk() 
     root.geometry(f"{WINDOW_STANDBY_WIDTH}x{WINDOW_STANDBY_HEIGHT}")
+
+    isColonDisplayed = True
+
     # 待機状態のフレーム
     frameStandby = tk.Frame(root, width=WINDOW_STANDBY_WIDTH, height=WINDOW_STANDBY_HEIGHT)
     isStatusBarShown = True
@@ -42,6 +66,8 @@ if __name__ == '__main__':
     frameCounting = tk.Frame(root, width=WINDOW_COUNTING_WIDTH, height=WINDOW_COUNTING_HEIGHT)
     buttonStop = tk.Button(frameCounting, text="停止", command=stopCounting )
     buttonStop.place(x=0,y=0)
+    labelTime = tk.Label(frameCounting, text="00:00", font=f"{SYSTEM_FONT_FAMILY} {SYSTEM_FONT_SIZE}", anchor="n")
+    labelTime.place(x=0,y=50)
 
     frameCounting.place(x=0, y=0)
     frameStandby.place(x=0, y=0)
