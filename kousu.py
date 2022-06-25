465ab524a11564b353dd4dfb0604dee070e73f55
ken551 <hide.delorean@gmail.com>
2022-06-25 20:08:21 +0900

    ボタンを押すと画面サイズとステータスバーの表示がトグルするように設定

--- kousu.py
+++ kousu.py
@@ -3,12 +3,14 @@ import tkinter as tk
 def toggleStatusBar():
     global isStatusBarShown
     if isStatusBarShown:
+        root.geometry("200x150")
         # ステータスバー表示
         root.overrideredirect(1)
         isStatusBarShown = False
         # 最前面固定
         root.attributes("-topmost", True)
     else:
+        root.geometry("400x300")
         # ステータスバー表示
         root.overrideredirect(0)
         isStatusBarShown = True
@@ -20,11 +22,11 @@ def onExit():
 
 if __name__ == '__main__':
     root = tk.Tk() 
+    root.geometry("400x300")
     isStatusBarShown = True
-    # ステータスバー非表示
-    root.overrideredirect(0)
     btnToggleStatusBar = tk.Button(root, text="i am a button", command=toggleStatusBar)
     btnToggleStatusBar.place(x=10,y=10)
     btnExit = tk.Button(root,text="exit", command=onExit)
     btnExit.place(x=100,y=100)
+    
     root.mainloop() 
